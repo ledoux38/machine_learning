717d343e733c04cfd79da7a94cb135898f94245d
ledoux <ledoux.florian30@gmail.com>
2017-02-06 22:40:19 +0100

    creation de l'interface graphique de base

--- code/package/Frame_journal.py
+++ code/package/Frame_journal.py
@@ -2,17 +2,27 @@
 #-*-coding:UTF-8 -*
 
 import Main_class
+from tkinter import *
 
-"""
-class Test(Main_class):
+class Main_frame():
 	def __init__(self):
-		Main_class.__init__(self)
-		self.text = str()
 
-	def ecriture(self):
-		i = 0
+		self.fenetre = Tk()
+		self.panel_principal = PanedWindow(self.fenetre, orient=VERTICAL)
+		self.panel_principal.pack(side=TOP, expand=Y, fill=BOTH, pady=2, padx=2)
+		self.text = Text(self.panel_principal,background = 'white')
+		self.panel_principal.add(self.text)
 
-		while(i<10):
-			text = input()
-			self._set_log(text)
-			i += 1
+		self.panel_boutton = PanedWindow(self.fenetre, orient = HORIZONTAL)
+		self.bp_button_ok = Button(self.panel_boutton, text = "Ok")
+		self.bp_button_RAZ = Button(self.panel_boutton, text = "RAZ")
+
+		self.panel_boutton.add(self.bp_button_ok)
+		self.panel_boutton.add(self.bp_button_RAZ)
+
+		self.panel_principal.add(self.panel_boutton)
+
+		self.panel_principal.pack()
+		self.fenetre.mainloop()
+
+a = Main_frame()
