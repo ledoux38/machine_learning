bcd3d4a3ab893cfa7f71de2267fbbc5b6df4cad2
ledoux <ledoux.florian30@gmail.com>
2017-02-06 21:38:32 +0100

    suppression de l'attribut donnée pour une sauvegarde des donnée directement dans un fichier

--- code/package/Main_class.py
+++ code/package/Main_class.py
@@ -5,15 +5,18 @@ class Main_class:
 	"""classe qui permet la recuperation de donnée ou de status des attributs"""
 	def __init__(self):
 		"""constructeur de la classe"""
-		self._donnee = str()
 
 	def _set_log(self,text):
 		if not isinstance(text,str):
 			raise TypeError("erreur parametre n'est pas de type str")
-		self._donnee += text + "\n"
+		text += "\n"
+		with open('../journal/fichier.txt', 'a') as mon_fichier:
+			mon_fichier.write(text)
 
 	def _get_log(self):
-		return self._donnee
+		with open('../journal/fichier.txt', 'r') as mon_fichier:
+			texte = mon_fichier.read()
+		return texte
 
 	donnee = property(_get_log,_set_log)
 
