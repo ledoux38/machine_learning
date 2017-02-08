8845a72daa9ec50c98552bbe57a1334798d529bd
ledoux <ledoux.florian30@gmail.com>
2017-02-08 22:24:04 +0100

    modification de la classe log, suppression de donnee obsolete

--- code/package/log.py
+++ code/package/log.py
@@ -1,24 +1,22 @@
 #!usr/bin/python3.5
 #-*-coding:UTF-8 -*
 
-class Main_class:
+class Log:
 	"""classe qui permet la recuperation de donnée ou de status des attributs"""
 	def __init__(self):
 		"""constructeur de la classe"""
-
-	def _set_log(self,text):
+	def set_log(self,text):
 		if not isinstance(text,str):
 			raise TypeError("erreur parametre n'est pas de type str")
 		text += "\n"
 		with open('./journal/fichier.txt', 'a') as mon_fichier:
 			mon_fichier.write(text)
 
-	def _get_log(self):
+	def get_log(self):
 		with open('./journal/fichier.txt', 'r') as mon_fichier:
 			texte = mon_fichier.read()
 		return texte
 
-	donnee = property(_get_log,_set_log)
 
 
 
