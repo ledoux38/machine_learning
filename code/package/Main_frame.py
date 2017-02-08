fe829eadda0d0d5687e54ce4cad0a6c7e1f2baf1
ledoux <ledoux.florian30@gmail.com>
2017-02-08 18:06:29 +0100

    nouvelle heritage de Main_frame (Frame de tkinter)

--- code/package/Main_frame.py
+++ code/package/Main_frame.py
@@ -5,6 +5,54 @@ from Main_class import *
 from tkinter import *
 
 
+class Main_frame(Main_class,Frame):
+	def __init__(self,fenetre,**kwargs):
+		Frame.__init__(self, fenetre, width=768, height=576, **kwargs)
+		self.pack(fill=BOTH)
+		Main_class.__init__(self)
+
+		self.panel_principal = PanedWindow(self, orient=VERTICAL)
+		self.panel_principal.pack(side=TOP, expand=Y, fill=BOTH, pady=2, padx=2)
+		self.canvas = Canvas(self.panel_principal,width = 150, height = 120,background = 'yellow')
+
+		self.panel_principal.add(self.canvas)
+
+		self.panel_button = PanedWindow(self,orient = HORIZONTAL)
+
+		self.button_fermer = Button(self.panel_button, text = "Fermer", command = self.quit)
+		self.button_generer = Button(self.panel_button, text = "Generer")
+		self.button_recommencer = Button(self.panel_button, text = "Recommencer")
+		self.button_option = Button(self.panel_button, text = "Option")
+
+		self.panel_button.add(self.button_option)
+		self.panel_button.add(self.button_recommencer)
+		self.panel_button.add(self.button_generer)
+		self.panel_button.add(self.button_fermer)
+
+		self.panel_principal.add(self.panel_button)
+
+		self.panel_principal.pack()
+
+
+
+
+
+
+
+
+
+fenetre = Tk()
+interface = Main_frame(fenetre)
+
+interface.mainloop()
+interface.destroy()
+
+
+
+
+
+
+"""
 class Main_frame(Main_class):
 	def __init__(self):
 		Main_class.__init__(self)
@@ -35,11 +83,4 @@ class Main_frame(Main_class):
 		self.fenetre.mainloop()
 
 
-
-
-
-
-
-
-
-a = Main_frame()
+"""
