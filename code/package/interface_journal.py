093a0ab9dbeb3927d835a671abaf9ceba46c7b26
ledoux <ledoux.florian30@gmail.com>
2017-02-09 21:35:46 +0100

    resolution de bug de compilation

--- code/package/interface_journal.py
+++ code/package/interface_journal.py
@@ -5,15 +5,15 @@ from log import *
 from tkinter import *
 
 class Interface_journal(Frame):
-	def __init__(self,Frame):
+	def __init__(self,fenetre,**kwargs):
 		Frame.__init__(self, fenetre, width=768, height=576, **kwargs)
 		self.pack(fill=BOTH)
-		self.panel_principal = PanedWindow(self.fenetre, orient=VERTICAL)
+		self.panel_principal = PanedWindow(self, orient=VERTICAL)
 		self.panel_principal.pack(side=TOP, expand=Y, fill=BOTH, pady=2, padx=2)
 		self.text = Text(self.panel_principal,background = 'white')
 		self.panel_principal.add(self.text)
 
-		self.panel_boutton = PanedWindow(self.fenetre, orient = HORIZONTAL)
+		self.panel_boutton = PanedWindow(self, orient = HORIZONTAL)
 		self.bp_button_ok = Button(self.panel_boutton, text = "Ok")
 		self.bp_button_RAZ = Button(self.panel_boutton, text = "RAZ")
 
@@ -23,29 +23,10 @@ class Interface_journal(Frame):
 		self.panel_principal.add(self.panel_boutton)
 
 		self.panel_principal.pack()
-
-
-#a = Frame_journal()
 
 """
-class Frame_journal():
-	def __init__(self):
-
-		self.fenetre = Tk()
-		self.panel_principal = PanedWindow(self.fenetre, orient=VERTICAL)
-		self.panel_principal.pack(side=TOP, expand=Y, fill=BOTH, pady=2, padx=2)
-		self.text = Text(self.panel_principal,background = 'white')
-		self.panel_principal.add(self.text)
-
-		self.panel_boutton = PanedWindow(self.fenetre, orient = HORIZONTAL)
-		self.bp_button_ok = Button(self.panel_boutton, text = "Ok")
-		self.bp_button_RAZ = Button(self.panel_boutton, text = "RAZ")
-
-		self.panel_boutton.add(self.bp_button_ok)
-		self.panel_boutton.add(self.bp_button_RAZ)
-
-		self.panel_principal.add(self.panel_boutton)
-
-		self.panel_principal.pack()
-		self.fenetre.mainloop()
+b = Tk()
+a = Interface_journal(b)
+a.mainloop()
+a.destroy()
 """
