187862e0d99add3e0ec8a9ec2f3047f6172ac0a6
ledoux <ledoux.florian30@gmail.com>
2017-02-09 21:22:03 +0100

    resolution de bug d'importation

--- code/package/log.py
+++ code/package/log.py
@@ -1,10 +1,30 @@
 #!usr/bin/python3.5
 #-*-coding:UTF-8 -*
-
+	
 class Log:
 	"""classe qui permet la recuperation de donnée ou de status des attributs"""
+	@staticmethod
+	def set_log(text):
+		if not isinstance(text,str):
+			raise TypeError("erreur parametre n'est pas de type str")
+		text += "\n"
+		with open('./journal/fichier.txt', 'a') as mon_fichier:
+			mon_fichier.write(text)
+
+	def get_log():
+		with open('./journal/fichier.txt', 'r') as mon_fichier:
+			texte = mon_fichier.read()
+		return texte
+
+
+
+"""
+
+class Log:
+
 	def __init__(self):
-		"""constructeur de la classe"""
+
+        @staticmethod
 	def set_log(self,text):
 		if not isinstance(text,str):
 			raise TypeError("erreur parametre n'est pas de type str")
@@ -18,24 +38,7 @@ class Log:
 		return texte
 
 
+Log.set_log("test")
+print(Log.get_log())
 
-
-"""
-class Test(Main_class):
-	def __init__(self):
-		Main_class.__init__(self)
-		self.text = str()
-
-	def ecriture(self):
-		i = 0
-
-		while(i<10):
-			text = input()
-			self._set_log(text)
-			i += 1
-
-
-a = Test()
-a.ecriture()
-print(a._get_log())
 """
