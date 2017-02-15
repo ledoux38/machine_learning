72b54ac3b4d29ffbd49cc6488a200f6140b15505
ledoux <ledoux.florian30@gmail.com>
2017-02-15 22:29:42 +0100

    changement de type de creation de dessin (passage par ligne au lieu de carrée)

--- code/package/canvas.py
+++ code/package/canvas.py
@@ -1,16 +1,27 @@
-#!usr/bin/python3.5
+#!usr/bin/python3.5c 
 #-*-coding:UTF-8 -*
 
 from tkinter import *
 
 class interface_canvas(Canvas):
-	def __init__ (self, parent, hauteur = 28, longueur = 28):
+	def __init__ (self, parent, hauteur = 48, longueur = 48):
 		Canvas.__init__(self, parent, height = hauteur, width = longueur)
 
 		self.bind("<B1-Motion>", self.creation_forme)
+		self.bind("<ButtonRelease>",self.reset_position)
+		self.old_position = (0,0)
 
 	def creation_forme(self,event):
-		self.create_rectangle((event.x,event.y),(event.x,event.y),fill = 'black')
+		if self.old_position == (0,0):
+			self.old_position = (event.x,event.y)
+
+		else:
+			self.create_line(self.old_position,(event.x,event.y))
+			self.old_position = (event.x,event.y)
+		#print(i,self.index(i,0))
+
+	def reset_position(self,event):
+		self.old_position = (0,0)
 
 	def tout_supprimer(self):
 		self.delete(ALL)
@@ -21,3 +32,25 @@ if __name__ == "__main__":
 	canvas.pack()
 
 	app.mainloop()
+
+"""
+class interface_canvas(Canvas):
+	def __init__ (self, parent, hauteur = 48, longueur = 48):
+		Canvas.__init__(self, parent, height = hauteur, width = longueur)
+
+		self.bind("<B1-Motion>", self.creation_forme)
+
+	def creation_forme(self,event):
+		i = self.create_rectangle((event.x,event.y),(event.x,event.y),fill = 'black')
+		#print(i,self.index(i,0))
+
+	def tout_supprimer(self):
+		self.delete(ALL)
+
+if __name__ == "__main__":
+	app = Tk()
+	canvas = interface_canvas(app, 100, 100)
+	canvas.pack()
+
+	app.mainloop()
+"""
