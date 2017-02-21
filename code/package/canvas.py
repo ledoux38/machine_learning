5fdb25584116ed324d78c02a4201320a3bad4444
ledoux <ledoux.florian30@gmail.com>
2017-02-21 22:27:48 +0100

    implementation de l'integration des tableau de numpy

--- code/package/canvas.py
+++ code/package/canvas.py
@@ -2,23 +2,37 @@
 #-*-coding:UTF-8 -*
 
 from tkinter import *
+from numpy import *
+
 
 class interface_canvas(Canvas):
 	def __init__ (self, parent, hauteur = 48, longueur = 48, outline = "black"):
 		Canvas.__init__(self, parent, height = hauteur, width = longueur)
 
+		self.canvas_hauteur = hauteur
+		self.canvas_longueur = longueur
+
 		self.bind("<B1-Motion>", self.creation_forme)
 		self.bind("<ButtonRelease>",self.reset_position)
 		self.old_position = (0,0)
+		self.tableau = zeros((hauteur,longueur),dtype = 'i')
 
 	def creation_forme(self,event):
 		if self.old_position == (0,0):
 			self.old_position = (event.x,event.y)
 
 		else:
-			self.create_line(self.old_position,(event.x,event.y))
+			i = self.create_line(self.old_position,(event.x,event.y))
 			self.old_position = (event.x,event.y)
-		#print(i,self.index(i,0))
+
+		variable_x = self.coords(i)
+		variable_x = int(variable_x[0])
+
+		variable_y = self.coords(i)
+		variable_y = int(variable_y[1])
+
+		self.tableau[variable_x][variable_y] = 1
+
 
 	def reset_position(self,event):
 		self.old_position = (0,0)
@@ -26,9 +40,19 @@ class interface_canvas(Canvas):
 	def tout_supprimer(self):
 		self.delete(ALL)
 
+	def get_tableau(self):
+		return self.tableau
+
+	def reset_tableau(self):
+		self.tableau = zeros((self.canvas_hauteur ,self.canvas_longueur),dtype = 'i')
+
+
+
+
 if __name__ == "__main__":
 	app = Tk()
 	canvas = interface_canvas(app, 100, 100)
+	canvas.reset_tableau()
 	canvas.pack()
 
 	app.mainloop()
