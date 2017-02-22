903ebb37c29289df9f05369505d36d7743234e77
ledoux <ledoux.florian30@gmail.com>
2017-02-22 13:37:22 +0100

    rajout fonction creation_tableau dans le canvas

--- code/package/canvas.py
+++ code/package/canvas.py
@@ -16,22 +16,28 @@ class interface_canvas(Canvas):
 		self.bind("<ButtonRelease>",self.reset_position)
 		self.old_position = (0,0)
 		self.tableau = zeros((hauteur,longueur),dtype = 'i')
+		self.nb_item = 0
 
 	def creation_forme(self,event):
 		if self.old_position == (0,0):
 			self.old_position = (event.x,event.y)
 
 		else:
-			i = self.create_line(self.old_position,(event.x,event.y))
+			self.nb_item = self.create_line(self.old_position,(event.x,event.y))
 			self.old_position = (event.x,event.y)
 
-		variable_x = self.coords(i)
-		variable_x = int(variable_x[0])
+	def creation_tableau(self):
+		compteur = 1
+		while compteur <= self.nb_item:
+			variable_x = self.coords(compteur)
+			variable_x = int(variable_x[0])
 
-		variable_y = self.coords(i)
-		variable_y = int(variable_y[1])
+			variable_y = self.coords(compteur)
+			variable_y = int(variable_y[1])
 
-		self.tableau[variable_x][variable_y] = 1
+			self.tableau[variable_x][variable_y] = 1
+			#print("id: ", compteur, "x: ", variable_x, "y: ", variable_y, self.tableau[variable_x][variable_y])
+			compteur += 1
 
 
 	def reset_position(self,event):
@@ -52,7 +58,6 @@ class interface_canvas(Canvas):
 if __name__ == "__main__":
 	app = Tk()
 	canvas = interface_canvas(app, 100, 100)
-	canvas.reset_tableau()
 	canvas.pack()
 
 	app.mainloop()
