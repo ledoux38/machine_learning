2abc7b6424d6e391954e29ae45ece71abb5f34b1
ledoux <ledoux.florian30@gmail.com>
2017-02-26 11:08:12 +0100

    -suppresion des morceaus de code obsoletes - maintien de la methode de sauvegarde du cavenas au cas ou on voudrais sauvegarder l'image - nouvelle methode de sauvegarde temp, cette fonction a pour but de creer un fichier temporaire pour pouvoir recuperer le tableau du canevas

--- code/package/canvas.py
+++ code/package/canvas.py
@@ -1,29 +1,26 @@
-#!usr/bin/python3.5c 
+#!usr/bin/python3.5
 #-*-coding:UTF-8 -*
 
 from tkinter import *
 from numpy import *
+from PIL import Image
+from PIL import ImageDraw
 
 
 class interface_canvas(Canvas):
 	def __init__ (self, parent, hauteur = 48, longueur = 48, outline = "black"):
 		Canvas.__init__(self, parent, height = hauteur, width = longueur)
 
-		self.canvas_hauteur = hauteur
-		self.canvas_longueur = longueur
-
 		self.bind("<B1-Motion>", self.creation_forme)
 		self.bind("<ButtonRelease>", self.reset_position)
 		self.old_position = (0,0)
-		
-		self.nb_item = 0
 
 	def creation_forme(self, event):
 		if self.old_position == (0,0):
 			self.old_position = (event.x, event.y)
 
 		else:
-			self.nb_item = self.create_line(self.old_position, (event.x,event.y), capstyle = 'round')
+			self.create_line(self.old_position, (event.x,event.y))
 			self.old_position = (event.x, event.y)
 
 
@@ -32,29 +29,22 @@ class interface_canvas(Canvas):
 
 	def tout_supprimer(self):
 		self.delete(ALL)
-		self.nb_item = 0
 
-	def get_tableau(self):
-		tableau = zeros((int(self.cget('height')), int(self.cget('width'))), dtype = 'i')
-		compteur = 1
-		while compteur <= self.nb_item:
-			variable_x = self.coords(compteur)
-			variable_x = int(variable_x[0])
 
-			variable_y = self.coords(compteur)
-			variable_y = int(variable_y[1])
-
-			tableau[variable_x][variable_y] = 1
-			compteur += 1
-		return tableau
-
-	def sauvegarde_canvas(self, filename):
+	def sauv_canvas(self, filename, type_img):
 
 		if not isinstance(filename, str):
 			raise TypeError("error file is not {} is this str ".format(type(filename)))
 
-		self.postscript(file="fichier.ps")
+		image = Image.new("RGB", (int(self.cget('height')), int(self.cget('width'))), "white")
+		draw = ImageDraw.Draw(image)
 
+		for x in self.find_all():
+			draw.line((self.coords(x)), fill = "black")
+		del draw
+		image.save(filename, type_img)
+
+	def sauv_canvas_tmp(self, filename, type_img):
 
 
 if __name__ == "__main__":
