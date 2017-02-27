e2d2e8190b9243e80723c0be689513e07782a0ff
ledoux <ledoux.florian30@gmail.com>
2017-02-27 21:39:01 +0100

    integration systeme PIL <experimental>

--- code/package/canvas.py
+++ code/package/canvas.py
@@ -1,6 +1,7 @@
 #!usr/bin/python3.5
 #-*-coding:UTF-8 -*
 
+from tkinter.filedialog import *
 from tkinter import *
 from numpy import *
 from PIL import Image
@@ -8,7 +9,15 @@ from PIL import ImageDraw
 
 
 class interface_canvas(Canvas):
+	"""
+	canvas personaliser pour dessiner des lignes
+	"""
+
 	def __init__ (self, parent, hauteur = 48, longueur = 48, outline = "black"):
+		"""
+		initialise la class:
+		"""
+
 		Canvas.__init__(self, parent, height = hauteur, width = longueur)
 
 		self.bind("<B1-Motion>", self.creation_forme)
@@ -16,6 +25,10 @@ class interface_canvas(Canvas):
 		self.old_position = (0,0)
 
 	def creation_forme(self, event):
+		"""
+		methode de classe qui creer des lignes dans le canevas
+		"""
+
 		if self.old_position == (0,0):
 			self.old_position = (event.x, event.y)
 
@@ -25,16 +38,24 @@ class interface_canvas(Canvas):
 
 
 	def reset_position(self, event):
+		"""
+		methode de classe qui reset la derniere position de la souris lors du tracage de la ligne
+		"""
+
 		self.old_position = (0,0)
 
 	def tout_supprimer(self):
+		"""
+		methode de classe qui supprime tout les items
+		"""
+
 		self.delete(ALL)
 
 
-	def sauv_canvas(self, filename, type_img):
-
-		if not isinstance(filename, str):
-			raise TypeError("error file is not {} is this str ".format(type(filename)))
+	def image(self):
+		"""
+		methode de classe qui redessine le canvas dans une image vierge
+		"""
 
 		image = Image.new("RGB", (int(self.cget('height')), int(self.cget('width'))), "white")
 		draw = ImageDraw.Draw(image)
@@ -42,10 +63,23 @@ class interface_canvas(Canvas):
 		for x in self.find_all():
 			draw.line((self.coords(x)), fill = "black")
 		del draw
-		image.save(filename, type_img)
+		return image
+
+
+	def sauvegarder(self, obj_image):
+		"""
+		methode de classe qui sauvegarde le dessin
+		"""
+
+		save = asksaveasfilename(defaultextension = '.JPG', initialdir = '/home/ledoux/Images/')
+		obj_image.save(save)
 
 	def sauv_canvas_tmp(self, filename, type_img):
+		"""
+		methode de classe qui permet de sauvegarder temporairement les images
+		"""
 
+		pass
 
 if __name__ == "__main__":
 	app = Tk()
