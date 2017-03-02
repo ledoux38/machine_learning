a1255a24e84417ec9c62bb0c56c59fff7b850fcc
ledoux <ledoux.florian30@gmail.com>
2017-03-02 13:34:09 +0100

    changement nom class interface_option en class option creation methode interface_option et transferes du code de la methode d'initialisation vers interface option

--- code/package/option.py
+++ code/package/option.py
@@ -7,14 +7,92 @@ from tkinter import *
 from interface_journal import*
 import tkinter.messagebox
 
-class Interface_option(Tk):
+class Options:
 	def __init__(self,parent):
-		Tk.__init__(self,parent)
-		self.title("Options")
-		self.initialize()
-		self.resizable(False,False)
+		self.app = Tk()
+		self.interface_option()
+
+
+	def interface_option(self):
+
+		self.app.title("Options")
+		self.app.resizable(False,False)
+
+		self.app.grid()
+
+		frame_ch_accees = LabelFrame(self.app, text = "gestion des chemins d'accées", padx = 5, pady = 5)
+
+		label_ch_sauv_image = Label(frame_ch_accees, text = "Chemin d'accès image: ")
+		self.app.entry_ch_accees_image = Entry(frame_ch_accees)
+
+		label_ch_sauv_image.grid(row = 0, column = 0)
+		self.app.entry_ch_accees_image.grid(row = 0, column = 1, sticky = 'EW')
+
+		label_ch_log = Label(frame_ch_accees, text = "Chemin d'accès log: ")
+		self.app.entry_ch_log = Entry(frame_ch_accees)
+
+		label_ch_log.grid(row = 1, column = 0, sticky = 'E')		
+		self.app.entry_ch_log.grid(row = 1, column = 1, sticky = 'EW')		
+
+		frame_ch_accees.grid(row = 0, column = 0, sticky = "EW")
+
+
+		frame_opt_canvas = LabelFrame(self.app, text = "gestion de la table de dessin", padx = 5, pady = 5)
+
+		self.app.value_long = IntVar(frame_opt_canvas)
+		scale_long = Scale(frame_opt_canvas,from_= 18, to = 50, showvalue = False, variable = self.app.value_long, orient = 'h')
+		entry_long = Entry(frame_opt_canvas, textvariable = self.app.value_long, width = 10)
+		label_long = Label(frame_opt_canvas, text = "longueur canvas: ")
+
+		label_long.grid(row = 0, column = 0, sticky = 'E')
+		scale_long.grid(row = 0, column = 1)
+		entry_long.grid(row = 0, column = 2)
+
+		self.app.value_hot = IntVar(frame_opt_canvas)
+		scale_hot = Scale(frame_opt_canvas,from_= 18, to = 50, showvalue = False, variable = self.app.value_hot, orient = 'h')
+		entry_hot = Entry(frame_opt_canvas, textvariable = self.app.value_hot, width = 10)		
+		label_hot = Label(frame_opt_canvas, text = "hauteur canvas: ")
+
+		label_hot.grid(row = 1, column = 0, sticky = 'E')
+		scale_hot.grid(row = 1, column = 1)
+		entry_hot.grid(row = 1, column = 2)
+
+		self.app.value_epais = IntVar(frame_opt_canvas)
+		scale_epais = Scale(frame_opt_canvas,from_= 1, to = 5, showvalue = False, variable = self.app.value_epais, orient = 'h')
+		entry_epais = Entry(frame_opt_canvas, textvariable = self.app.value_epais, width = 10)		
+		label_epais = Label(frame_opt_canvas, text = "épaisseur canvas: ")
+		
+		label_epais.grid(row = 2, column = 0, sticky = 'E')
+		scale_epais.grid(row = 2, column = 1)
+		entry_epais.grid(row = 2, column = 2)
+
+
+		frame_opt_canvas.grid(row = 1, column = 0, sticky = 'EW')
+
+		frame_bp_opt = Frame(self.app)
+
+		self.app.bp_journal = Button(frame_bp_opt, text = "journal",command = self.ouvrir_journal)
+		self.app.bp_journal.grid(row=0,column=0,sticky='W')
+
+		frame_bp_opt.grid(row = 2, column = 0, sticky = "EW")
+
+		frame_bp_command_inter = Frame(self.app)
+
+		self.app.bp_appliquer = Button(frame_bp_command_inter, text = "Appliquer", command = self.sauv_configuration)
+		self.app.bp_appliquer.grid(row=0,column=0,sticky='EW')
+
+		self.app.bp_quit = Button(frame_bp_command_inter, text = "Quitter",command = self.quitter_interface)
+		self.app.bp_quit.grid(row=0,column=1,sticky='EW')
+		
+		frame_bp_command_inter.grid(row = 3, column = 0, sticky = "EW")
+
+		self.app.grid_columnconfigure(0,weight=1)
+		self.app.grid_rowconfigure(0,weight=0)
+		self.app.grid_rowconfigure(2,weight=1)
+		self.app.mainloop()
 
 	def initialize(self):
+		"""
 		self.grid()
 
 		frame_ch_accees = LabelFrame(self, text = "gestion des chemins d'accées", padx = 5, pady = 5)
@@ -86,12 +164,14 @@ class Interface_option(Tk):
 		self.grid_columnconfigure(0,weight=1)
 		self.grid_rowconfigure(0,weight=0)
 		self.grid_rowconfigure(2,weight=1)
+		"""
+		pass
 
 	def sauv_configuration(self):
 		tkinter.messagebox.showinfo("Information","Boutton <Appliquer> non implémenté \n Prochainement!!")
 
 	def quitter_interface(self):
-		self.destroy()
+		self.app.quit()
 
 	def ouvrir_journal(self):
 		inter_journal = Interface_journal(None)
@@ -100,7 +180,6 @@ class Interface_option(Tk):
 		print("canvas longueur:", self.value_long.get(), "canvas hauteur:", self.value_hot.get(), "canvas epaisseur:", self.value_epais.get())
 
 if __name__ == "__main__":
-	app = Interface_option(None)
-	app.get_param()
-	app.mainloop()
+	app = Options(None)
+
 
