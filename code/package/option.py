b23578948f58304ed269ee2202608b2b674208a4
ledoux <ledoux.florian30@gmail.com>
2017-03-04 15:27:48 +0100

    phase 2 des modification du programme realisé avec succes les modification sont: - suoression des codes obsoletes -rajout et modification des attribut

--- code/package/option.py
+++ code/package/option.py
@@ -53,30 +53,6 @@ class Options:
 		dic = {"ch_img":"/img", "ch_log":"/journal", "h_canvas":18, "l_canvas":18, "e_t_canvas":1}
 		return dic
 
-	def get_param(self):
-		"""
-		methode de class qui renvoi les attributs des objet principaux de la classe
-		"""		
-		dic = {"ch_img":self.app.entry_ch_accees_image.get(), "ch_log":self.app.entry_ch_log.get(), "h_canvas":self.app.value_hot.get(), "l_canvas":self.app.value_long.get(), "e_t_canvas":self.app.value_epais.get()}
-		return dic
-
-	def set_param(self, option):
-		"""
-		methode de class qui modifie directement les attributs de la class
-		"""
-
-		self.app.entry_ch_accees_image.insert(0, option["ch_img"])
-		self.app.entry_ch_log.insert(0, option["ch_log"])
-		self.app.value_hot.set(option["h_canvas"])
-		self.app.value_long.set(option["l_canvas"])
-		self.app.value_epais.set(option["e_t_canvas"])
-
-	def afficher(self):
-		"""
-		methode de class qui affiche la fenetre
-		"""
-
-		self.app.mainloop()
 
 	def initialize(self):
 		"""
@@ -91,48 +67,48 @@ class Options:
 		frame_ch_accees = LabelFrame(app, text = "gestion des chemins d'accées", padx = 5, pady = 5)
 
 		label_ch_sauv_image = Label(frame_ch_accees, text = "Chemin d'accès image: ")
-		app.entry_ch_accees_image = Entry(frame_ch_accees)
-		app.entry_ch_accees_image.insert(0, self.ch_img)
+		entry_ch_accees_image = Entry(frame_ch_accees)
+		entry_ch_accees_image.insert(0, self.ch_img)
 
 		label_ch_sauv_image.grid(row = 0, column = 0)
-		app.entry_ch_accees_image.grid(row = 0, column = 1, sticky = 'EW')
+		entry_ch_accees_image.grid(row = 0, column = 1, sticky = 'EW')
 
 		label_ch_log = Label(frame_ch_accees, text = "Chemin d'accès log: ")
-		app.entry_ch_log = Entry(frame_ch_accees)
-		app.entry_ch_log.insert(0, self.ch_log)
+		entry_ch_log = Entry(frame_ch_accees)
+		entry_ch_log.insert(0, self.ch_log)
 
 		label_ch_log.grid(row = 1, column = 0, sticky = 'E')		
-		app.entry_ch_log.grid(row = 1, column = 1, sticky = 'EW')		
+		entry_ch_log.grid(row = 1, column = 1, sticky = 'EW')		
 
 		frame_ch_accees.grid(row = 0, column = 0, sticky = "EW")
 
 
 		frame_opt_canvas = LabelFrame(app, text = "gestion de la table de dessin", padx = 5, pady = 5)
 
-		app.value_long = IntVar(frame_opt_canvas)
-		app.value_long.set(self.l_canvas)
-		scale_long = Scale(frame_opt_canvas,from_= 18, to = 50, showvalue = False, variable = app.value_long, orient = 'h')
-		entry_long = Entry(frame_opt_canvas, textvariable = app.value_long, width = 10)
+		value_long = IntVar(frame_opt_canvas)
+		value_long.set(self.l_canvas)
+		scale_long = Scale(frame_opt_canvas,from_= 18, to = 50, showvalue = False, variable = value_long, orient = 'h')
+		entry_long = Entry(frame_opt_canvas, textvariable = value_long, width = 10)
 		label_long = Label(frame_opt_canvas, text = "longueur canvas: ")
 
 		label_long.grid(row = 0, column = 0, sticky = 'E')
 		scale_long.grid(row = 0, column = 1)
 		entry_long.grid(row = 0, column = 2)
 
-		app.value_hot = IntVar(frame_opt_canvas)
-		app.value_hot.set(self.h_canvas)
-		scale_hot = Scale(frame_opt_canvas,from_= 18, to = 50, showvalue = False, variable = app.value_hot, orient = 'h')
-		entry_hot = Entry(frame_opt_canvas, textvariable = app.value_hot, width = 10)		
+		value_hot = IntVar(frame_opt_canvas)
+		value_hot.set(self.h_canvas)
+		scale_hot = Scale(frame_opt_canvas,from_= 18, to = 50, showvalue = False, variable = value_hot, orient = 'h')
+		entry_hot = Entry(frame_opt_canvas, textvariable = value_hot, width = 10)		
 		label_hot = Label(frame_opt_canvas, text = "hauteur canvas: ")
 
 		label_hot.grid(row = 1, column = 0, sticky = 'E')
 		scale_hot.grid(row = 1, column = 1)
 		entry_hot.grid(row = 1, column = 2)
 
-		app.value_epais = IntVar(frame_opt_canvas)
-		app.value_epais.set(self.e_t_canvas)
-		scale_epais = Scale(frame_opt_canvas,from_= 1, to = 5, showvalue = False, variable = app.value_epais, orient = 'h')
-		entry_epais = Entry(frame_opt_canvas, textvariable = app.value_epais, width = 10)		
+		value_epais = IntVar(frame_opt_canvas)
+		value_epais.set(self.e_t_canvas)
+		scale_epais = Scale(frame_opt_canvas,from_= 1, to = 5, showvalue = False, variable = value_epais, orient = 'h')
+		entry_epais = Entry(frame_opt_canvas, textvariable = value_epais, width = 10)		
 		label_epais = Label(frame_opt_canvas, text = "épaisseur canvas: ")
 		
 		label_epais.grid(row = 2, column = 0, sticky = 'E')
@@ -151,7 +127,7 @@ class Options:
 
 		frame_bp_command_inter = Frame(app)
 
-		app.bp_appliquer = Button(frame_bp_command_inter, text = "Appliquer", command = self.sauv_configuration)
+		app.bp_appliquer = Button(frame_bp_command_inter, text = "Appliquer", command = partial(self.sauv_configuration, entry_ch_accees_image, entry_ch_log, value_long, value_hot, value_epais))
 		app.bp_appliquer.grid(row=0,column=0,sticky='EW')
 
 		app.bp_quit = Button(frame_bp_command_inter, text = "Quitter",command = partial(self.quitter_interface, app))
@@ -165,12 +141,12 @@ class Options:
 
 		app.mainloop()
 
-	def sauv_configuration(self):
+	def sauv_configuration(self, ch_img, ch_log, value_long, value_hot, value_epais):
 		"""
 		methode de class qui sauvegarde la nouvelle configuration
 		"""
-
-		self.sauvegarde_opt(self.get_param())
+		dic = {"ch_img":ch_img.get(), "ch_log":ch_log.get(), "h_canvas":value_hot.get(), "l_canvas":value_long.get(), "e_t_canvas":value_epais.get()}
+		self.sauvegarde_opt(dic)
 
 	def quitter_interface(self, fenetre):
 		"""
