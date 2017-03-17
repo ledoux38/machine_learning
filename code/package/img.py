78884c09c214962bd99bf896ca14f7b25fe1c8c8
ledoux <ledoux.florian30@gmail.com>
2017-03-17 22:21:11 +0100

    modif des opt pour valeur par def rajout opt ep_canvas modif fonct sauve_img integration opt modif prog pour standard imposé

--- code/package/img.py
+++ code/package/img.py
@@ -13,29 +13,24 @@ class img:
 										 - Sauvegarde du dessin dans un dossier specifique
 										 - Sauvegarde dans un fichier temporaire 
 	"""
-	
-	def __init__(self, obj_canvas = None, list_option = ['/home/ledoux/Documents/Programmation/python/python-le-on/proj/machine_learning/code/numero/', '.JPG']):
-		"""
-		if not isinstance(obj_canvas, Canvas):
-			raise TypeError("erreur obj_canvas = {} n'est pas de type Canvas ".format(type(obj_canvas)))
-		"""
-		if not isinstance(list_option, list):
-			raise TypeError("erreur option = {} n'est pas de type list ".format(type(list_option)))
 
 
-		self.option = list_option
-		"""
-		liste les options données: [0] chemin par defauts
-								   [1] type par defauts
-		"""
+	def __init__(self, obj_canvas = None, option = {"ch_img":"./img", "ch_log":"./log/activity.log", "h_canvas":18, "l_canvas":18, "e_t_canvas":1}):
+		
+		if not isinstance(option, dict):
+			raise TypeError("erreur option = {} n'est pas de type list ".format(type(option)))
+
+		#liste les options
+		self.option = option
+
+		#creation d'une image pillow vierge
 		if obj_canvas is None:
 			self._image = None
 		else:
 			self._image = self.creation_image(obj_canvas)
-		"""
-		creation d'une image pillow vierge
-		"""
+
 	
+
 	def __repr__(self):
 		"""
 		methode de class pour la visualisation du tableau via l'interpreteur
@@ -54,6 +49,8 @@ class img:
 				u = 0
 				i += "\n"
 		return i
+
+
 
 	def __str__(self):
 		"""
@@ -75,6 +72,7 @@ class img:
 		return i
 
 
+
 	def creation_image(self, obj_canvas):
 		"""
 		creer une image via pillow
@@ -83,51 +81,54 @@ class img:
 		if not isinstance(obj_canvas, Canvas):
 			raise TypeError("erreur obj_canvas = {} n'est pas de type Canvas ".format(type(obj_canvas)))
 
-		"""
-		creation d'une image pillow vierge
-		"""
+		#creation d'une image pillow vierge
 		image = Image.new("RGB", (int(obj_canvas.cget('height')), int(obj_canvas.cget('width'))), "white")
 		draw = ImageDraw.Draw(image)
 		
-		""" 
-		ecriture sur l'image pillow
-		"""
+		#ecriture sur l'image pillow
 		for x in obj_canvas.find_all():
-			draw.line((obj_canvas.coords(x)), fill = "black")
+			draw.line((obj_canvas.coords(x)), fill = "black", width = self.option["e_t_canvas"])
 
 		del draw
 		return image
 	
+
+
 	def _get_image(self):
 		"""
 		retourne l'image 
 		"""
+
 		return self._image
+
+
 
 	def _set_image(self, obj_canvas):
 		"""
 		modification de l'image 
 		"""
+
 		if not isinstance(obj_canvas, Canvas):
 			raise TypeError("erreur obj_canvas = {} n'est pas de type Canvas ".format(type(obj_canvas)))
 
 		self._image = self.creation_image(obj_canvas)
+
+
 
 	def sauv_img(self):
 		"""
 		proposition a l'utilisateur de sauvegarder sont image
 		"""
 		
-		save = asksaveasfilename(defaultextension = self.option[1], initialdir = self.option[0])
-		"""
-		si sauvegarde annuler alors return sinon sauvegarde
-		"""
-
+		save = asksaveasfilename(defaultextension = ".JPG", initialdir = self.option["ch_img"])
+		
+		#si sauvegarde annuler alors return sinon sauvegarde
 		if not save:
 			return None
 		else:
 			new_img = self.image.resize((28,28))
-			self.image.save(save)
+			self._image = new_img
+			self._image.save(save)
 			return save
 
 
@@ -136,21 +137,24 @@ class img:
 		"""
 		methode de classe qui retourne une list de l'image
 		"""
-
+		new_img = self.image.resize((28,28))
+		self._image = new_img
 		return list(self._image.getdata())
 
 	image = property(_get_image, _set_image)
+
+
 
 from canvas import *
 if __name__ == "__main__":
 
 	
 	app = Tk()
-	appcanvas = interface_canvas(app, hauteur = 5, longueur = 5)
+	appcanvas = interface_canvas(app)
 	appcanvas.create_line(0, 0, 4, 4)
 	im = img(appcanvas)
 	i = im.get_data()
-	print(im)
+	im.sauv_img()
 	appcanvas.pack()
 	app.mainloop()
 	
