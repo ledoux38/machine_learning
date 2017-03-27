02af3000a60a78a26b394c35a6f4f00dc2751257
ledoux <ledoux.florian30@gmail.com>
2017-03-27 22:14:51 +0200

    correctif et changement du retour des fonction return inutile

--- code/package/utilitaire_debug.py
+++ code/package/utilitaire_debug.py
@@ -25,28 +25,27 @@ def print_array(array):
 					else:
 						temp += " {} ".format(array[i,j])
 				temp += "\n"
-			return temp
+			print(temp)
 
 		#sinon si array et un tableau de numpy
-		if isinstance(array, np.ndarray):
+		elif isinstance(array, np.ndarray):
 			if not np.shape(array) == (28,28):
-				print("non")
 				array = np.reshape(array, (28, 28))
 			for i,u in enumerate(array):
 				for j,k in enumerate(array[i]):
 					temp += " {} ".format(array[i,j])
 				temp += "\n"
-		return temp
+		print(temp)
 
 	except:
 		print("erreur!! affichage standard", type(array))
 		print(array)
 
-def print_array_convert(array, valeur1 = "#", valeur2 = " "):
+def print_array_convert(array, valeur_principale_a_remplacer = "#", valeur = 1, autre_valeur = " ", fin = ""):
 	array = np.reshape(array, (28, 28))
 	for i in range(28):
 		for j in range(28):
-			print(valeur1 if array[i,j] == 1 else valeur2, end= "")
+			print(valeur_principale_a_remplacer if array[i,j] == int(valeur) else autre_valeur, end= fin)
 		print("")
 
 
@@ -54,15 +53,15 @@ def print_array_convert(array, valeur1 = "#", valeur2 = " "):
 if __name__ == "__main__":
 	i = np.eye(28, dtype = np.float64)
 	print (type(i))
-	print(print_array(i))
+	print_array(i)
 
 	i = np.ones((28,28), dtype = np.float64)
 	print (type(i))
-	print(print_array(i))
+	print_array(i)
 
 	i = range(784)
 	print (type(i))
-	print(print_array(i))
+	print_array(i)
 
 	i = list()
 	temp = 0
@@ -71,9 +70,13 @@ if __name__ == "__main__":
 		temp += 1
 
 	print (type(i))
-	print(print_array(i))
+	print_array(i)
 
 
 	i = np.ones((784), dtype = np.float64)
 	print (type(i))
-	print(print_array(i))
+	print_array(i)
+
+	i = np.ones((28,28), dtype = np.float64)
+	print (type(i))
+	print_array_convert(i)
