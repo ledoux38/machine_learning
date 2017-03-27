4409b765899f0c2d0bffaffd62a78188e05c443d
ledoux <ledoux.florian30@gmail.com>
2017-03-27 13:06:18 +0200

    modification de la  fonctions tableau en print_array

--- code/package/utilitaire_debug.py
+++ code/package/utilitaire_debug.py
@@ -5,20 +5,22 @@ import numpy as np
 
 def tableau(array):
 	temp = str("")
+
+	#si c'est une list ou un range
 	if isinstance(array, list) or isinstance(array, range):
-		print("if")	
-		for i in range(28):
-			for j in range(28):
-				temp += " {} ".format(array[i,j])
-				if j == 27:
-					temp += "\n"
-	else:
-		print("else")		
+		redim = np.reshape(array, (28, 28))
+		for i,u in enumerate(redim):
+			for j,k in enumerate(redim[i]):
+				temp += " {} ".format(redim[i,j])
+			temp += "\n"
+
+	#sinon si array et un tableau de numpy
+	elif isinstance(array, np.ndarray):	
 		for i,u in enumerate(array):
 			for j,k in enumerate(array[i]):
 				temp += " {} ".format(array[i,j])
 			temp += "\n"
-		return temp
+	return temp
 
 
 
@@ -46,13 +48,20 @@ if __name__ == "__main__":
 	i = np.eye(28)
 	print (type(i))
 	print(tableau(i))
-	#print(tableauv2(i))
 
 	i = np.ones((28,28))
 	print (type(i))
 	print(tableau(i))
-	#print(tableauv2(i))
 
 	i = range(784)
+	print (type(i))
+	print(tableau(i))
+
+	i = list()
+	temp = 0
+	while temp < 784:
+		i.append(temp)
+		temp += 1
+
 	print (type(i))
 	print(tableau(i))
